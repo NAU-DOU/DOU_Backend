e29f0fb41580bd75aac677b21682c5b3fc795bad
silverage <sep.non2@gmail.com>
2024-05-01 03:21:05 +0900

    :sparkles: [FEAT]: 감정 분석 모델 Serving 관련 Flask 코드 작성 (#1)

--- DOU_Flask/app.py
+++ DOU_Flask/app.py
@@ -1,18 +1,27 @@
-from flask import Flask
-import json
+from flask import Flask, request, jsonify
+from model.predict import evaluation_predict
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return "This is home!"
+def rootFlask():
+  return "This is ROOT of Flask"
 
-@app.route('/sentiment')
+@app.route('/health-check')
+def healthCheck():
+  return "Flask is Healthy."
+
+@app.route('/sentiment', methods=['POST'])
 def sentiment():
-  result = {
-    "test": "test"
-  }
-  return json.dumps(result)
+  req = request.get_json()
+
+  result = []
+
+  for sentence in req['sentence']:
+    sent_result = evaluation_predict(sentence)
+    result.append(sent_result)
+
+  return jsonify(data=result)
 
 if __name__ == '__main__':
   app.run('localhost', port=5000, debug=True)
