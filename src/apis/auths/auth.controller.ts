2533a23ee6579c9d33b28445f282e3885403fa09
silvarge <sep.non2@gmail.com>
2024-11-06 00:42:37 +0900

    📝 [Add] 카카오 소셜로그인 관련 Swagger Docs 내용 추가
    소셜로그인 관련 스웨거 꾸밈!

--- src/apis/auths/auth.controller.ts
+++ src/apis/auths/auth.controller.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { KakaoRequest } from './interface/kakao.interface';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCookieAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LogoutAuthOutputDto } from './dto/user.auth.dto';
 import { CurrentUser } from './dto/current.user.decorator';
 import { JwtAuthGuard, JwtKakaoAuthGuard } from './strategies/kakao.strategy';
@@ -15,17 +15,28 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   /**
-   * 인가 코드 요청
+   * ## 카카오 인가 코드 요청 API
+   *
+   * - **주의:** 아래 링크를 직접 클릭하여 접근해야 함
+   * - Swagger의 **GET 요청**이나 **Postman/Insomnia 등**을 통해 요청하는 것은 동작하지 않을 확률이 높음
+   *
+   * [인가 코드 요청](https://dev.nau-dou.shop/oauth/kakao)
    */
-  // TODO: kakao/authorize - 인가 코드 요청
+  @ApiTags('Kakao Auth (카카오 소셜로그인)')
+  @ApiOperation({ summary: '카카오 소셜로그인 트리거 API (스웨거 동작 X)' })
   @Get('kakao')
   @UseGuards(AuthGuard('kakao'))
   async kakaoAuth(@Req() request: Request) {}
 
   /**
-   * 토큰 발급
+   * ## 토큰 발급
+   *
+   * - **주의:** **oauth/kakao**를 실행하면 해당 route로 **자동적으로 callback**되는 API
+   * - **Swagger나 Postman/Insomnia 등**을 통해 요청하면 오류만 뜨고 말 것임
    */
   // TODO: kakao/token - 토큰 발급
+  @ApiTags('Kakao Auth (카카오 소셜로그인)')
+  @ApiOperation({ summary: '카카오 소셜로그인 콜백 API (사용X)' })
   @Get('kakao/callback')
   @UseGuards(AuthGuard('kakao'))
   async kakaoAuthCallback(
@@ -40,8 +51,15 @@ export class AuthController {
   }
 
   /**
-   * 로그인 연장
+   * ## 로그인 연장 (리프레시 토큰 요청 API)
+   *
+   * - 보낼 때 Cookie를 함께 보내야 함 (헤더의 쿠키!)
+   * - Cookie에 보낼 때는, **eid_refresh_token: "refresh token 값"**
+   * - ### 실제 Swagger UI 에서는 쿠키를 테스트할 수가 없어서 **브라우저나 Postman/Insomnia로 직접 쿠키를 포함**해서 테스트 해야 함!
    */
+  @ApiTags('Kakao Auth (카카오 소셜로그인)')
+  @ApiCookieAuth('eid_refresh_token') // 쿠키 인증 추가
+  @ApiOperation({ summary: '리프레시 토큰 요청 API' })
   @Post('kakao/refresh')
   async silentKakaoRefresh(@Req() request: Request, @Res({ passthrough: true }) response: Response) {
     const result = await this.authService.silentKakaoRefresh(request, response);
@@ -52,8 +70,12 @@ export class AuthController {
   }
 
   /**
-   * 로그아웃
+   * ## 로그아웃
+   * - Bearer <JWT 토큰> 형식으로 AWT 토큰 전달이 필요해용
    */
+  @ApiTags('Kakao Auth (카카오 소셜로그인)')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '카카오 로그아웃 API' })
   @Post('kakao/logout')
   @UseGuards(JwtKakaoAuthGuard)
   async kakaoLogout(@Res({ passthrough: true }) response: Response, @CurrentUser() user) {
